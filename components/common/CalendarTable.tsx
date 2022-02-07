c8546c1e0eaae368011c02593be4466e09232df8
tamagram <tamadevelopment211@gmail.com>
2022-02-07 20:27:59 +0900

    :sparkles: Support holidays.

--- components/common/CalendarTable.tsx
+++ components/common/CalendarTable.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import styles from "./CalendarTable.module.scss"
 
 type CalendarTableProps = {
@@ -11,13 +12,25 @@ const today = new Date();
 
 
 const CalendarTable: React.FC<CalendarTableProps> = ({ year, month }) => {
+  const [holidays, setHolidays] = useState({} as { [yyyymmdd: string]: string })
+
+  useEffect(() => {
+    fetch(`https://holidays-jp.github.io/api/v1/${year}/date.json`)
+      .then((response) => {
+        response.json().then((data) => {
+          setHolidays(data);
+          console.dir(data)
+        })
+      }).catch((err) => {
+        console.error(err)
+      })
+  }, [year])
+
   const DaysTr = () => {
     const daysData = (): string[] => {
       const offset = new Date(year, month - 1, 1).getDay()
-      console.log(year, month, offset)
       let tableInMonth = Array(offset).fill("null")
       tableInMonth = tableInMonth.concat(Array.from(Array(days[month - 1]), (_, i) => ((i + 1).toString())))
-      console.dir(tableInMonth)
       tableInMonth = tableInMonth.concat(Array(tableCellLength - tableInMonth.length).fill("null"))
       return tableInMonth
     }
@@ -27,7 +40,11 @@ const CalendarTable: React.FC<CalendarTableProps> = ({ year, month }) => {
       const tr = (
         <tr key={i}>
           {daysInMonth.slice(i * 7, (i + 1) * 7).map((day, j) => {
-            if (day === today.getDate().toString() && month === today.getMonth() + 1 && year === today.getFullYear()) return <td className={styles.CalendarTable__td__Today} key={j}>{day}</td>
+            console.log(`${year}-${('0' + (month + 1)).slice(-2)}-${('0' + (i * 7 + j)).slice(-2)}`)
+            if (day === today.getDate().toString() &&
+              month === today.getMonth() + 1 &&
+              year === today.getFullYear()) return <td className={styles.CalendarTable__td__Today} key={j}>{day}</td>
+            else if (holidays[`${year}-${('0' + month).slice(-2)}-${('0' + (day)).slice(-2)}`]) return <td className={styles.CalendarTable__td__Holiday} key={j}>{day}</td>
             else if (j === 0) return <td className={styles.CalendarTable__td__Sunday} key={j}>{day}</td>
             else if (j === 6) return <td className={styles.CalendarTable__td__Saturday} key={j}>{day}</td>
             else return <td className={styles.CalendarTable__td} key={j}>{day}</td>
@@ -39,7 +56,7 @@ const CalendarTable: React.FC<CalendarTableProps> = ({ year, month }) => {
     return <>{trs}</>
   }
 
-  return (
+  return (<>
     <table className={styles.CalendarTable}>
       <thead>
         <tr>
@@ -56,6 +73,7 @@ const CalendarTable: React.FC<CalendarTableProps> = ({ year, month }) => {
         <DaysTr />
       </tbody>
     </table>
+  </>
   )
 }
 
